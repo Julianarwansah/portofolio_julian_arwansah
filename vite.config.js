e52a4acb25832c49706f3f5e77f03d145bc92bbc
NamaAnda <julianarwansahh@gmail.com>
2025-12-02 16:03:25 +0700

    feat: Update deployment paths and build script for Windows compatibility.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: "/portofolio_julian_arwansah/",
   plugins: [react(), tailwindcss()],
 })
