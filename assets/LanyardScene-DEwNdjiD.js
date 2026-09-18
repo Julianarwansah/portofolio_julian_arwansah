9a6aa82b75dfbca259303e630c7b027dc2d4699e
Julian Arwansah <julianarwansahh@gmail.com>
2026-09-18 10:20:50 +0700

    Updates

--- assets/LanyardScene-DEwNdjiD.js
+++ assets/LanyardScene-DEwNdjiD.js
@@ -1,4 +1,4 @@
-import{r as pl,g as Kk,R as RI,a as nA,j as KI,_ as JF}from"./index-yWI7FF91.js";var dh={exports:{}},KQ={};/**
+import{r as pl,g as Kk,R as RI,a as nA,j as KI,_ as JF}from"./index-BOoOyGYf.js";var dh={exports:{}},KQ={};/**
  * @license React
  * react-reconciler-constants.production.js
  *
