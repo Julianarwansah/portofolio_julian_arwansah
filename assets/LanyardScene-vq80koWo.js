82894adb1672d98c79cbb0f96e6ed3aa87a30e44
Julian Arwansah <julianarwansahh@gmail.com>
2026-09-23 10:50:13 +0700

    Updates

--- assets/LanyardScene-vq80koWo.js
+++ assets/LanyardScene-vq80koWo.js
@@ -1,4 +1,4 @@
-import{r as og,g as GA,R as Sb,a as we,j as Ot}from"./index-1DF5uqR_.js";var xp={exports:{}},Xs={};/**
+import{r as og,g as GA,R as Sb,a as we,j as Ot}from"./index-DdYSsQZR.js";var xp={exports:{}},Xs={};/**
  * @license React
  * react-reconciler-constants.production.js
  *
